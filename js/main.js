b5e970353c38db498df26cda74d441267754dd9f
Adam Aisenberg <103009405+panyyoga@users.noreply.github.com>
2022-09-21 19:23:43 +0300

    littleChanges

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 'use strict'
 
-var gLevel = { SIZE: 4, MINES: 1 }
+var gLevel = {
+    SIZE: 3,
+    MINES: 1
+}
+// var gLevel = { SIZE: 3, MINES: 1 }
 var gBoard
 
 const MINE = '💣'
@@ -10,26 +14,25 @@ const ON_GAME = '😀'
 const LOSE = '😵'
 const WIN = '😎'
 
-var gGame = { 
+var gGame = {
 
-    isOn: true, 
-    shownCount: 0, 
+    isOn: false,
+    shownCount: 0,
     markedCount: 0,
-    secsPassed: 0 
+    secsPassed: 0
 }
+
+var IsFirstClick = false
 
 function initGame() {
-    gGame = 
-        { 
-        isOn: true, 
-        shownCount: 0, 
-        markedCount: 0, 
-        secsPassed: 0 
-    }
+    gGame.isOn = true
     gBoard = buildBoard(gLevel.SIZE)
     document.querySelector('.reset').innerHTML = ON_GAME
-    renderBoard(gBoard)
+    renderBoard(gBoard) 
 }
+
+
+showTimer()
 
 function buildBoard(size) {
     var board = [];
@@ -52,15 +55,58 @@ function buildBoard(size) {
 function renderBoard(board) {
     var strHTML = `<table border="2px"><tbody>`;
     for (var i = 0; i < board.length; i++) {
-        strHTML += '<tr>';
+        strHTML += '<tr>\n';
         for (var j = 0; j < board[0].length; j++) {
-            strHTML += `<td><button class="cell"></button></td>`
+            strHTML += `<td>\n
+            <button class="cell"></button>
+            <div class="cell${i}-${j}"><div>\n
+            </td>\n`
         }
-        strHTML += `</tr>`
+        strHTML += `</tr>\n`
     }
-    strHTML += `</tbody></table>`;
-    var elContainer = document.querySelector('.board-container');
-    console.log(elContainer);
-    elContainer.innerHTML = strHTML;
+    strHTML += `\n</tbody>\n</table>\n`
+
+    // console.log(strHTML);
+    var elContainer = document.querySelector('.board-container')
+    console.log(elContainer)
+    elContainer.innerHTML = strHTML
 }
 
+
+function showTimer(){
+    var elModal=document.querySelector('.timerModal')
+    
+    
+    if(IsFirstClick===true){
+        IsFirstClick = false
+        return elModal.style.display='block'
+    }else{
+
+    }
+    
+}
+
+
+
+
+
+
+
+
+// chzooseLevel(SIZE = 3) {
+
+//     if (SIZE === 3) {
+//         //change '.gameFrame' style
+//     }
+//     if (SIZE === 4) {
+//         //change '.gameFrame' style
+
+//     }if (SIZE === 5) {
+//         //change '.gameFrame' style
+//     }
+
+
+//     return number
+// }
+
+// // getHint(){}
